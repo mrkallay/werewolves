835ea6826e55b9b6b5d2cd7104e92c3b84ed384a
Brian Kallay <brian.kallay@gmail.com>
2020-05-08 20:15:15 -0700

    added player

---
+++ src/Player.java
@@ -0,0 +1,29 @@
+/**
+ * Created by briankallay on 5/8/20.
+ */
+public class Player
+{
+    private String name;
+    //Character character;
+    private boolean isDoppleganger;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public boolean isDoppleganger()
+    {
+        return isDoppleganger;
+    }
+
+    public void setDoppleganger(boolean doppleganger)
+    {
+        isDoppleganger = doppleganger;
+    }
+}
